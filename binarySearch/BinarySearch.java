b7a560e97706779be8e881232ade69573fe8f5b5
Marcelo Henrique <mhbcoelho99@gmail.com>
2023-05-19 13:21:58 -0300

    BinarySearch in java

---
+++ binarySearch/BinarySearch.java
@@ -0,0 +1,20 @@
+package binarySearch;
+
+public class BinarySearch {
+
+    public BinarySearch() {}
+    
+    public static int search(int key, int a[]) {
+        return search(key, a, 0, a.length-1);
+    }
+
+    private static int search(int key, int a[], int lo, int hi) {
+        if(lo > hi)  return -1;
+        int mid = lo + (hi - lo) / 2;
+
+        if(key < a[mid]) return search(key, a, lo, mid - 1);
+        else if(key > a[mid]) return search(key, a, mid + 1, hi);
+        else return mid;
+    }
+    
+}
