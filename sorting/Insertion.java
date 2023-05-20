d491eab016d1a2322667ee3dd32906e0ac7ba47a
Marcelo Henrique <mhbcoelho99@gmail.com>
2023-05-20 10:59:47 -0300

    Insertion sort

--- sorting/Insertion.java
+++ sorting/Insertion.java
@@ -1,5 +1,30 @@
 package sorting;
 
 public class Insertion {
+
+    public static <T extends Comparable<T>> void sort(T[] a) {
+        int N = a.length;
+
+        for (int i = 1; i < N; i++) { 
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--)
+                exch(a, j, j - 1);
+        }
+    }
+
+    private static <T extends Comparable<T>> boolean less(T v, T w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static <T extends Comparable<T>> void exch(T[] a, int i, int j) {
+        T t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static void main(String[] args) {
+        Integer []numbers = {2, 3, 4, 6, 9, 1};
+
+        Insertion.sort(numbers);
     
+    }
 }
