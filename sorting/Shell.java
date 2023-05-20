ee9e50349ef266efce3f17e3dc5291d7398e8302
Marcelo Henrique <mhbcoelho99@gmail.com>
2023-05-20 15:30:35 -0300

    Shell sort

--- sorting/Shell.java
+++ sorting/Shell.java
@@ -1,5 +1,28 @@
 package sorting;
 
 public class Shell {
-    
+    public static <T extends Comparable<T>> void sort(T[] a) { 
+        int N = a.length;
+        int h = 1;
+        while (h < N / 3)
+            h = 3 * h + 1;  // 1, 4, 13, 40, 121, 364, 1093, ...
+        while (h >= 1) { 
+            for (int i = h; i < N; i++) { 
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h)
+                    exch(a, j, j - h);
+            }
+            h = h / 3;
+        }
+    }
+
+    private static <T extends Comparable<T>> boolean less(T v, T w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static <T extends Comparable<T>> void exch(T[] a, int i, int j) {
+        T t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
 }
