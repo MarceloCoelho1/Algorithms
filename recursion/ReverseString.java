2f5dfa11089d0e628e9332049e9a525d81454b32
Marcelo Henrique <mhbcoelho99@gmail.com>
2023-05-20 18:45:01 -0300

    ReverseString

--- recursion/ReverseString.java
+++ recursion/ReverseString.java
@@ -3,7 +3,17 @@ package recursion;
 public class ReverseString {
 
     public static String reverseString(String input) {
+        // what is the base case?
+        if(input.equals("")) {
+            return "";
+        }
+        
+        // what is the smallest amount of work i can do in each iteration;
+        return reverseString(input.substring(1)) + input.charAt(0);
+    }
 
-        return null;
+    public static void main(String[] args) {
+        String response = ReverseString.reverseString("marcelo");
+        System.out.println(response);
     }
 }
