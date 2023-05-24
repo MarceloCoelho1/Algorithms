5d8153819972b94d621dc313a882cfa38830b8c2
Marcelo <mhbcoelho99@gmail.com>
2023-05-24 10:45:48 -0300

    InterviewExercices

---
+++ interviewExercices/CommonItemsInArray.java
@@ -0,0 +1,24 @@
+package interviewExercices;
+
+
+public class CommonItemsInArray {
+    /*
+     * Given 2 arrays, create a function that let's user know (true / false)
+     * whether these two arrays contain any common items
+     * 
+     * for example: 
+     * array1 = {a, b, c, d}
+     * array2 = {z, x, i}
+     * return false;
+     * 
+     * Your code will be considered correct if it meets the following criteria:
+     * 1: it's readble
+     * 2: Good time complexity
+     * 3: Good space complexity
+     */
+
+    public static boolean commonItemsInArray(String[] vector1, String[] vector2) {
+
+        return false;
+    }
+}
