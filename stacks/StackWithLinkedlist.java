c22a0ddf62e424c28d83ecfabc4402bf793ef91d
Marcelo <mhbcoelho99@gmail.com>
2023-05-26 06:25:47 -0300

    StacksWithLinkedList

--- stacks/StackWithLinkedlist.java
+++ stacks/StackWithLinkedlist.java
@@ -1,5 +1,33 @@
 package stacks;
 
 public class StackWithLinkedlist {
-    
+    private Integer length;
+    private Node bottom;
+    private Node top;
+
+    public StackWithLinkedlist() {
+        this.length = -1;
+    }
+
+    public void peek(){}
+
+    public void push(Integer value){
+        if(length == -1) {
+            Node newNode = new Node(value);
+            bottom = newNode;
+            top = newNode;
+        } else {
+            Node newNode = new Node(value);
+            newNode.next = top;
+            top = newNode;
+        }
+        length++;
+    }
+
+
+    public void pop(){}
+
+    public boolean isEmpty(){
+        return true;
+    }
 }
