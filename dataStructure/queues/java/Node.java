c558da539bddbc640f0d621f02af0fa704387477
Marcelo <mhbcoelho99@gmail.com>
2023-06-20 08:18:32 -0300

    queue in java

---
+++ dataStructure/queues/java/Node.java
@@ -0,0 +1,12 @@
+package queues.java;
+
+public class Node {
+    Integer value;
+    Node next;
+
+    public Node(Integer value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
